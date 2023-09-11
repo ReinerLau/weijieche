03e56d832c8a78da7b83fcef369579b6206353c6
ReinerLau <lk850593913@gmail.com>
2023-09-11 17:00:56 +0800

    feat: 初始化布局

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>首页</main>
+  <el-container class="h-full">
+    <el-header class="bg-slate-500">Header</el-header>
+    <el-container>
+      <el-aside class="bg-gray-500" width="100px">Aside</el-aside>
+      <el-main class="bg-zinc-400">Main</el-main>
+    </el-container>
+    <el-footer class="bg-red-500">Footer</el-footer>
+  </el-container>
 </template>
