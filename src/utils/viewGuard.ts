9bfbf7fd348f9b4d426e0cc3d8b8de211e027d9f
ReinerLau <lk850593913@gmail.com>
2023-09-11 13:10:27 +0800

    feat: token 校验

--- src/utils/viewGuard.ts
+++ src/utils/viewGuard.ts
@@ -1,20 +1,34 @@
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
-import type { Router } from 'vue-router'
+import type { RouteLocationNormalizedLoaded, Router } from 'vue-router'
+import { getToken } from './token'
+
+const whiteList = ['/login']
 
 export function checkToken(router: Router) {
   router.beforeEach((to) => {
     NProgress.start()
     document.title = '围界车'
-    // const hasToken = getToken()
-    // if (hasToken) {
-    //   return handleHasToken(to)
-    // } else {
-    //   return handleNoToken(to)
-    // }
+    const hasToken = getToken()
+    return hasToken ? handleHasToken(to) : handleNoToken(to)
   })
 
   router.afterEach(() => {
     NProgress.done()
   })
+}
+
+async function handleHasToken(to: RouteLocationNormalizedLoaded) {
+  if (to.path === '/login') {
+    return '/'
+  } else {
+    return true
+  }
+}
+
+function handleNoToken(to: RouteLocationNormalizedLoaded) {
+  if (!whiteList.includes(to.path)) {
+    return '/login'
+  }
+  return true
 }
