12850d961fcacc88886cc103f3bea024bd225ae1
ReinerLau <lk850593913@gmail.com>
2023-09-11 14:03:38 +0800

    feat: 登录 ui

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-[#2d3a4b] h-full flex justify-center flex-col items-center">
     <div class="text-white text-2xl font-bold mb-5">围界车</div>
-    <!-- <el-form
+    <el-form
       ref="loginFormRef"
       :model="loginForm"
       :rules="loginRules"
@@ -17,92 +17,88 @@
           type="text"
           tabindex="1"
           auto-complete="on"
-          @keydown.enter="loginForm.keydown"
+          @keydown.enter="handleKeydown"
         />
       </el-form-item>
 
       <el-form-item prop="password">
         <el-input
           v-model="loginForm.password"
-          :type="passwordType"
+          type="password"
           placeholder="密码"
           name="password"
           tabindex="2"
           auto-complete="on"
-          @keydown.enter="loginForm.keydown"
+          @keydown.enter="handleKeydown"
         />
       </el-form-item>
 
-      <el-checkbox
+      <!-- <el-checkbox
         v-model="ifRememberPassword"
         class="text-white block"
         label="记住密码"
         @change="handleRememberPassword"
-      ></el-checkbox>
+      ></el-checkbox> -->
 
-      <el-button
-        class="w-full"
-        :loading="loading"
-        type="primary"
-        @click.prevent="handleLogin"
-      >
+      <el-button class="w-full" :loading="loading" type="primary" @click.prevent="handleLogin">
         登录
       </el-button>
-    </el-form> -->
+    </el-form>
   </div>
 </template>
 
-<!-- <script setup lang="ts">
-import { reactive, ref } from "vue";
-import { useRouter } from "vue-router";
-import { useRememberPassword } from "../composables/useRememberPassword";
-import { useUserStore } from "../store/user";
-import { getCookie, setCookie } from "../utils/support";
-const { ifRememberPassword, handleRememberPassword, getRememberPassword } =
-  useRememberPassword();
+<script setup lang="ts">
+import type { FormInstance } from 'element-plus'
+import { reactive, ref, type Ref } from 'vue'
+import { useRouter } from 'vue-router'
+// import { useRememberPassword } from "../composables/useRememberPassword";
+// import { useUserStore } from "../store/user";
+// import { getCookie, setCookie } from "../utils/support";
+// const { ifRememberPassword, handleRememberPassword, getRememberPassword } =
+//   useRememberPassword();
 
 const loginForm = reactive({
-  username: "",
-  password: "",
-  keydown: (event: KeyboardEvent) => {
-    if (event.key === "Enter") {
-      handleLogin();
-    }
-  },
-});
+  username: '',
+  password: ''
+})
 const loginRules = {
-  username: [{ required: true, trigger: "blur", message: "用户名不能为空" }],
-  password: [{ required: true, trigger: "blur", message: "密码不能为空" }],
-};
-
-const passwordType = ref("password");
-
-loginForm.username = getCookie("username") || "";
-loginForm.password = getRememberPassword();
-
-const loading = ref(false);
-const loginFormRef = ref(null);
-const userStore = useUserStore();
-const router = useRouter();
-function handleLogin() {
-  loginFormRef.value.validate(async (valid) => {
-    if (valid) {
-      loading.value = true;
-      const { login } = userStore;
-      try {
-        await login(loginForm);
-        loading.value = false;
-        setCookie("username", loginForm.username, 15);
-        if (ifRememberPassword.value) {
-          setCookie("password", loginForm.password, 15);
-        }
-        router.push("/");
-      } catch {
-        loading.value = false;
-      }
-    } else {
-      return false;
-    }
-  });
+  username: [{ required: true, trigger: 'blur', message: '用户名不能为空' }],
+  password: [{ required: true, trigger: 'blur', message: '密码不能为空' }]
 }
-</script> -->
+
+// loginForm.username = getCookie("username") || "";
+// loginForm.password = getRememberPassword();
+
+const loading = ref(false)
+const loginFormRef: Ref<FormInstance | undefined> = ref()
+// const userStore = useUserStore();
+const router = useRouter()
+function handleLogin() {
+  loginFormRef.value &&
+    loginFormRef.value.validate(async (valid) => {
+      if (valid) {
+        loading.value = true
+        // const { login } = userStore
+        try {
+          // await login(loginForm)
+          // loading.value = false
+          // setCookie('username', loginForm.username, 15)
+          // if (ifRememberPassword.value) {
+          //   setCookie('password', loginForm.password, 15)
+          // }
+          router.push('/')
+        } finally {
+          loading.value = false
+        }
+      } else {
+        return false
+      }
+    })
+}
+
+function handleKeydown(e: KeyboardEvent) {
+  if (e.key === 'Enter') {
+    handleLogin()
+  }
+}
+</script>
