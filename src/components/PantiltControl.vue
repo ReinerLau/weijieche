3208d3532bcaf35514fe93a35868162f33659e03
ReinerLau <lk850593913@gmail.com>
2023-09-18 14:00:21 +0800

    feat: 云台控制

--- src/components/PantiltControl.vue
+++ src/components/PantiltControl.vue
@@ -4,29 +4,29 @@
     <div class="flex justify-center">
       <el-row class="w-48">
         <el-col :span="8" :offset="8">
-          <el-button size="large" class="w-full">
+          <el-button size="large" class="w-full" @click="onClick(keyMap.UP)">
             <i-bxs-up-arrow />
           </el-button>
         </el-col>
         <el-row class="w-full">
           <el-col :span="8">
-            <el-button size="large" class="w-full">
+            <el-button size="large" class="w-full" @click="onClick(keyMap.LEFT)">
               <i-bxs-left-arrow />
             </el-button>
           </el-col>
           <el-col :span="8">
-            <el-button size="large" class="w-full">
+            <el-button size="large" class="w-full" @click="onClick(keyMap.SWITCH)">
               <i-icomoon-free-switch />
             </el-button>
           </el-col>
           <el-col :span="8">
-            <el-button size="large" class="w-full">
+            <el-button size="large" class="w-full" @click="onClick(keyMap.RIGHT)">
               <i-bxs-right-arrow />
             </el-button>
           </el-col>
         </el-row>
         <el-col :span="8" :offset="8">
-          <el-button size="large" class="w-full">
+          <el-button size="large" class="w-full" @click="onClick(keyMap.DOWN)">
             <i-bxs-down-arrow />
           </el-button>
         </el-col>
@@ -35,12 +35,89 @@
     <div class="flex-1 flex flex-col justify-around">
       <div class="flex text-white items-center">
         <span class="w-24">水平角度</span>
-        <el-slider class="flex-1" :step="1" :min="-180" :max="180" :show-input-controls="false" />
+        <el-slider
+          v-model="horizonAngle"
+          class="flex-1"
+          :step="1"
+          :min="-180"
+          :max="180"
+          :show-input-controls="false"
+          @change="handleChangeAngle(angleTypes.HORIZON)"
+        />
       </div>
       <div class="flex text-white items-center">
         <span class="w-24">垂直角度</span>
-        <el-slider class="flex-1" :step="1" :min="-180" :max="180" :show-input-controls="false" />
+        <el-slider
+          v-model="verticalAngle"
+          class="flex-1"
+          :step="1"
+          :min="-180"
+          :max="180"
+          :show-input-controls="false"
+          @change="handleChangeAngle(angleTypes.HORIZON)"
+        />
       </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { patrolingCruise } from '@/api'
+import { debounce } from 'lodash'
+import { ref, type Ref } from 'vue'
+import { currentCar, haveCurrentCar } from '../shared/index'
+
+const horizonAngle = ref(0)
+const verticalAngle = ref(0)
+
+const angleTypes = {
+  HORIZON: 'horizon',
+  VERTICAL: 'vertical'
+}
+
+const keyMap = {
+  UP: '08',
+  LEFT: '04',
+  SWITCH: '255',
+  RIGHT: '02',
+  DOWN: '16'
+}
+
+function onClick(value: string) {
+  if (haveCurrentCar()) {
+    const data = {
+      code: currentCar.value,
+      param1: '02',
+      param2: value,
+      param3: 0,
+      param4: 0
+    }
+    patrolingCruise(data)
+  }
+}
+const changeHorizonAngle = createDebouce('75', horizonAngle)
+
+const changeVerticalAngle = createDebouce('77', verticalAngle)
+
+function createDebouce(param2: string, ref: Ref<number>) {
+  return debounce(async () => {
+    if (haveCurrentCar()) {
+      const data = {
+        code: currentCar.value,
+        param1: '01',
+        param2: param2,
+        param3: ref.value.toString(),
+        param4: 'ff'
+      }
+      patrolingCruise(data)
+    }
+  }, 500)
+}
+function handleChangeAngle(type: string) {
+  if (type === angleTypes.HORIZON) {
+    changeHorizonAngle()
+  } else if (type === angleTypes.VERTICAL) {
+    changeVerticalAngle()
+  }
+}
+</script>
