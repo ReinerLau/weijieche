7239cc9ce1373864005e10177f181d6d98088d4b
ReinerLau <lk850593913@gmail.com>
2023-11-10 16:09:52 +0800

    refactor: token 相关注释

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,15 +1,19 @@
 import Cookies from 'js-cookie'
 
+// token 前缀
 const TokenKey = 'weijieche_token'
 
+// 获取 token
 export function getToken() {
   return Cookies.get(TokenKey)
 }
 
+// 保存 token
 export function setToken(token: string) {
   return Cookies.set(TokenKey, token)
 }
 
+// 删除 token
 export function removeToken() {
   return Cookies.remove(TokenKey)
 }
