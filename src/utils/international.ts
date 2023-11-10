026630aeb6ed01643058641f44cb5f3b09986476
ReinerLau <lk850593913@gmail.com>
2023-11-10 16:09:29 +0800

    refactor: 国际化相关注释

--- src/utils/international.ts
+++ src/utils/international.ts
@@ -4,6 +4,8 @@ import zh from '@/locales/zh.json'
 import { createI18n } from 'vue-i18n'
 import { getCookie } from '.'
 
+// 导出s实例 i18n 给 vue 注册
+// https://vue-i18n.intlify.dev/guide/
 const i18n = createI18n({
   legacy: false,
   locale: getCookie('locale') || 'zh',
