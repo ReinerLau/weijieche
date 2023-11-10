633afc26fba92eb771d3fd2770e348583e13b80a
ReinerLau <lk850593913@gmail.com>
2023-11-10 16:09:10 +0800

    refactor: cookie 相关注释

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,13 +1,16 @@
 import Cookies from 'js-cookie'
 
+// 设置 cookie
 export function setCookie(key: string, value: string, expires?: number) {
   return Cookies.set(key, value, { expires })
 }
 
+// 获取 cookie
 export function getCookie(key: string) {
   return Cookies.get(key)
 }
 
+// 删除 cookie
 export function removeCookie(key: string) {
   return Cookies.remove(key)
 }
