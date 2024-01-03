7d084adde89961a7fcea06cd892886fcac832a39
hye0228 <1261769443@qq.com>
2024-01-03 17:37:55 +0800

    feat: 时间格式化

---
+++ src/utils/parseTime.ts
@@ -0,0 +1,27 @@
+export function parseTime(
+  time: string | number | Date | null | undefined,
+  cFormat?: string
+): string | null {
+  if (!time) {
+    return null
+  }
+  const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
+  const date = new Date(time)
+  const formatObj = {
+    y: date.getFullYear(),
+    m: date.getMonth() + 1,
+    d: date.getDate(),
+    h: date.getHours(),
+    i: date.getMinutes(),
+    s: date.getSeconds(),
+    a: date.getDay()
+  }
+  const time_str = format.replace(/{([ymdhisa])+}/g, (match, key) => {
+    const value = formatObj[key as keyof typeof formatObj]
+    if (key === 'a') {
+      return ['日', '一', '二', '三', '四', '五', '六'][value]
+    }
+    return value.toString().padStart(2, '0')
+  })
+  return time_str
+}
