e0bfa793353db81951f6511e3b8bc30f289baa08
hye0228 <1261769443@qq.com>
2024-01-19 17:22:18 +0800

    feat: 首页分析功能接口

--- src/api/charts.ts
+++ src/api/charts.ts
@@ -1,7 +1,17 @@
 import { request } from '@/utils'
 export function getPatrolData(startTime: any, endTime: any) {
   return request({
-    url: '/vehicle-task/v1/result/analyze',
+    url: '/result/analyze/v1/taskCount',
+    method: 'post',
+    params: {
+      startTime,
+      endTime
+    }
+  })
+}
+export function getErrorData(startTime: any, endTime: any) {
+  return request({
+    url: '/result/analyze/v1/errorCount',
     method: 'post',
     params: {
       startTime,
