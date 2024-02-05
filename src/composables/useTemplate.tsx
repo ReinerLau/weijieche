f1b486332a60295b497157d5a5126e242e1d2d8e
hye0228 <1261769443@qq.com>
2024-02-05 16:30:32 +0800

    refactor: 新增警报功能，查询列表功能

--- src/composables/useTemplate.tsx
+++ src/composables/useTemplate.tsx
@@ -1,16 +1,26 @@
-import { deleteTemplate, getTemplateList } from '@/api'
+import { deleteTemplate, getTemplatePathList } from '@/api'
 import {
   ElButton,
   ElDialog,
   ElForm,
   ElFormItem,
+  ElImage,
   ElInput,
+  ElPagination,
+  ElScrollbar,
   ElTable,
   ElTableColumn
 } from 'element-plus'
+import { cloneDeep } from 'lodash'
 import { defineComponent, ref, watch } from 'vue'
 import type { Ref } from 'vue'
 import { useI18n } from 'vue-i18n'
+
+// 重置表单数据
+const defaultFormData = {
+  name: '',
+  memo: ''
+}
 
 // 模板相关
 export const useTemplate = () => {
@@ -22,7 +32,10 @@ export const useTemplate = () => {
   const dialogVisible = ref(false)
 
   // 新建模板的表单数据
-  const formData: Ref<{ name?: string; memo?: string }> = ref({})
+  const formData: Ref<{
+    name?: string
+    memo?: string
+  }> = ref(cloneDeep(defaultFormData))
 
   // 搜索模板弹窗是否可见
   const searchDialogVisible = ref(false)
@@ -30,16 +43,22 @@ export const useTemplate = () => {
   // 警告弹窗是否可见
   const alarmDialogVisible = ref(false)
 
+  watch(dialogVisible, () => {
+    if (dialogVisible.value === false) {
+      formData.value = cloneDeep(defaultFormData)
+    }
+  })
+
   // 新建模板弹窗组件
   // https://cn.vuejs.org/guide/typescript/composition-api.html#without-script-setup
   const TemplateDialog = defineComponent({
     emits: ['confirm'],
     setup(props, { emit }) {
       return () => (
-        <ElDialog v-model={dialogVisible.value} title={t('mo-ban')}>
+        <ElDialog v-model={dialogVisible.value} title={t('mo-ban')} width="50vw" align-center>
           {{
             default: () => (
-              <ElForm label-width={250} model={formData.value}>
+              <ElForm label-width={100} model={formData.value}>
                 <ElFormItem prop="name" label={t('mo-ban-ming-cheng')}>
                   <ElInput v-model={formData.value.name} clearable></ElInput>
                 </ElFormItem>
@@ -87,38 +106,109 @@ export const useTemplate = () => {
           getList()
         }
       })
+      const queryFields = [
+        {
+          prop: 'name',
+          title: t('ren-wu-ming-cheng')
+        },
+        {
+          prop: 'memo',
+          title: t('bei-zhu')
+        }
+      ]
+
+      const initialParams = {
+        limit: 10,
+        page: 1,
+        rtype: 'patroling'
+      }
+
+      const params: Record<string, any> = ref(Object.assign({}, initialParams))
+
+      const total = ref(0)
 
       // 获取列表数据
       async function getList() {
-        const res = await getTemplateList({ limit: 999999, rtype: 'patroling' })
-        list.value = res.data || []
+        const res = await getTemplatePathList(params.value)
+        list.value = res.data.list || []
+        total.value = res.data ? res.data.total : 0
+      }
+
+      function handleQuery() {
+        getList()
+      }
+
+      function handleReset() {
+        params.value = Object.assign({}, initialParams)
+        getList()
       }
 
       return () => (
-        <ElDialog v-model={searchDialogVisible.value} title={t('mo-ban')} width="80%">
+        <ElDialog v-model={searchDialogVisible.value} title={t('mo-ban')} width="50vw" align-center>
           {{
+            header: () => (
+              <div class=" flex flex-col h">
+                <span class="mb-3">{t('lu-xian-mo-ban')}</span>
+                <ElScrollbar>
+                  <div class="flex justify-between">
+                    {queryFields.map((item) => (
+                      <div>
+                        <ElInput
+                          v-model={params.value[item.prop]}
+                          placeholder={item.title}
+                          clearable
+                        ></ElInput>
+                      </div>
+                    ))}
+                  </div>
+                </ElScrollbar>
+                <div class="flex pt-2">
+                  <ElButton type="primary" class="mr-2" onClick={handleQuery}>
+                    {t('cha-xun')}
+                  </ElButton>
+                  <ElButton type="info" onClick={handleReset}>
+                    {t('zhong-zhi')}
+                  </ElButton>
+                </div>
+              </div>
+            ),
             default: () => (
-              <ElTable
-                height="50vh"
-                data={list.value}
-                highlight-current-row
-                onCurrent-change={(val) => {
-                  currentTemplate.value = val
-                }}
-              >
-                <ElTableColumn property="name" label={t('ming-cheng')} />
-                <ElTableColumn property="memo" label={t('bei-zhu')} />
-                <ElTableColumn property="createTime" label={t('chuang-jian-shi-jian')} />
-                <ElTableColumn label={t('cao-zuo')}>
-                  {{
-                    default: ({ row }: { row: any }) => (
-                      <ElButton link onClick={() => handleDelete(row.id)}>
-                        {t('shan-chu')}
-                      </ElButton>
-                    )
+              <div class="flex flex-col ">
+                <ElTable
+                  height="50vh"
+                  data={list.value}
+                  highlight-current-row
+                  onCurrent-change={(val) => {
+                    currentTemplate.value = val
                   }}
-                </ElTableColumn>
-              </ElTable>
+                >
+                  <ElTableColumn property="name" label={t('ming-cheng')} />
+                  <ElTableColumn property="memo" label={t('bei-zhu')} />
+                  <ElTableColumn property="createTime" label={t('chuang-jian-shi-jian')} />
+                  <ElTableColumn label={t('cao-zuo')}>
+                    {{
+                      default: ({ row }: { row: any }) => (
+                        <ElButton link onClick={() => handleDelete(row.id)}>
+                          {t('shan-chu')}
+                        </ElButton>
+                      )
+                    }}
+                  </ElTableColumn>
+                </ElTable>
+                <div class="flex justify-end mt-2">
+                  <ElScrollbar class="mt-2">
+                    <ElPagination
+                      layout="sizes, prev, pager, next"
+                      total={total.value}
+                      v-model:current-page={params.value.page}
+                      v-model:page-size={params.value.limit}
+                      page-sizes={[10, 20, 50, 100, 200, 500]}
+                      onSize-change={getList}
+                      onCurrent-change={getList}
+                    />
+                  </ElScrollbar>
+                </div>
+              </div>
             ),
             footer: () => (
               <ElButton
@@ -137,21 +227,73 @@ export const useTemplate = () => {
   })
 
   //警报弹窗
+
+  async function handleAlarm() {
+    console.log('忽视')
+  }
   const TemplateAlarmDialog = defineComponent({
     emits: ['confirm'],
-    setup() {
+    props: {
+      wsdata: {
+        type: Object,
+        required: true
+      }
+    },
+    setup(props, { emit }) {
       // 每次打开搜索弹窗重新获取数据
       watch(alarmDialogVisible, async (val) => {
         if (val) {
           getList()
         }
       })
+      const srcList: any = ref([])
 
       // 获取图片数据
-      async function getList() {}
+      async function getList() {
+        // srcList.value.push(props.wsdata.picPath)
+        srcList.value.push('1.jpeg')
+      }
 
       return () => (
-        <ElDialog v-model={alarmDialogVisible.value} title="警告弹窗" width="80%"></ElDialog>
+        <ElDialog
+          v-model={alarmDialogVisible.value}
+          title={t('jing-bao-xiang-qing')}
+          class="h-[80vh]"
+          width="500"
+          align-center
+        >
+          {{
+            default: () => (
+              <div class="flex items-center justify-around ">
+                <span>{props.wsdata.message}</span>
+                <ElImage
+                  src="1.jpeg"
+                  fit="cover"
+                  zoom-rate={1.2}
+                  max-scale={7}
+                  min-scale={0.2}
+                  preview-src-list={srcList.value}
+                  initial-index={0}
+                />
+              </div>
+            ),
+            footer: () => (
+              <div class="flex justify-between">
+                <ElButton size="large" class="w-full" onClick={() => handleAlarm}>
+                  {t('bu-zuo-chu-li')}
+                </ElButton>
+                <ElButton
+                  size="large"
+                  type="primary"
+                  class="w-full"
+                  onClick={() => (alarmDialogVisible.value = false)}
+                >
+                  {t('shou-dong-chu-li')}
+                </ElButton>
+              </div>
+            )
+          }}
+        </ElDialog>
       )
     }
   })
@@ -161,6 +303,7 @@ export const useTemplate = () => {
     TemplateSearchDialog,
     searchDialogVisible,
     TemplateAlarmDialog,
-    alarmDialogVisible
+    alarmDialogVisible,
+    handleAlarm
   }
 }
