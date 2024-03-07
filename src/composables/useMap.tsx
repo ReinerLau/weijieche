e4c468e3ae52432251910fa6abee77b49903573b
hye0228 <1261769443@qq.com>
2024-03-07 13:27:55 +0800

    feat: 解决录制路线过于密集的问题

--- src/composables/useMap.tsx
+++ src/composables/useMap.tsx
@@ -140,11 +140,7 @@ export const useMap = () => {
       let pointNum: number = 0
       let clickNum: number = 0
       // 保存已有车速值
-      const carSpeedData = ref<
-        {
-          carNum: string
-        }[]
-      >([])
+      const carSpeedData = ref<string[]>([])
       // 每次点击地图新建路线点的事件
       function pathPointDrawendEvent(e: any) {
         pointNum++
@@ -224,17 +220,32 @@ export const useMap = () => {
         }
       }
 
+      const MAX_ZOOM = 25
+
+      function handleResolutions() {
+        const resolutions = []
+        const d = 2 * 6378137 * Math.PI
+        for (let i = 0; i < MAX_ZOOM; i++) {
+          resolutions[i] = d / (256 * Math.pow(2, i))
+        }
+
+        return resolutions
+      }
+
       // 初始化地图
       function initMap() {
         tileLayer = new maptalks.TileLayer('base', {
+          maxAvailableZoom: 19,
           urlTemplate: '/tiles/{z}/{x}/{y}.jpg',
           tileSystem: [1, 1, -20037508.34, -20037508.34]
         })
         if (mapRef.value) {
           map = new maptalks.Map(mapRef.value, {
             center: [113.48570073, 22.56210475],
+            spatialReference: {
+              resolutions: handleResolutions()
+            },
             zoom: 12,
-            maxZoom: 19,
             minZoom: 11,
             baseLayer: tileLayer
           })
@@ -464,8 +475,8 @@ export const useMap = () => {
 
       //设置速度
 
-      const pointsData = ref<string[]>([])
-      const pathPointsData = ref<string[]>([])
+      const pointsData = ref<{ x: number; y: number; speed: string }[]>([])
+      const pathPointsData = ref<{ x: number; y: number; speed: string }[]>([])
       function handleConfirmPointCarConfig(data: any) {
         if (pathPointsData.value.length !== 0) {
           pointsData.value = [...pathPointsData.value]
@@ -496,16 +507,12 @@ export const useMap = () => {
                 )
               })
             } else if (pathPointsData.value.length !== 0) {
-              if (missionTemplateId.value) {
-                const params = missionTemplateId.value
-                  ? {
-                      missionTemplateId: missionTemplateId.value
-                    }
-                  : {}
-                res = await sendMavlinkMission(pathPointsData.value, currentCar.value, params)
-              } else {
-                res = await sendMavlinkMission(pathPointsData.value, currentCar.value)
-              }
+              const params = missionTemplateId.value
+                ? {
+                    missionTemplateId: missionTemplateId.value
+                  }
+                : {}
+              res = await sendMavlinkMission(pathPointsData.value, currentCar.value, params)
               ElMessage.success({
                 message: res.message
               })
@@ -616,7 +623,7 @@ export const useMap = () => {
       let onePoint: maptalks.Marker | undefined
       const pathPointList: any = []
       // 确定选择模板路线在地图上显示
-      const missionTemplateId = ref<number>()
+      const missionTemplateId = ref<number | null | undefined>()
 
       function handleConfirmTemplate(template: any) {
         entryPoint = undefined
@@ -782,7 +789,7 @@ export const useMap = () => {
                     clickNum = index + 1
 
                     //保存已有车速值
-                    const carNum: string = carSpeedData.value[index] || ''
+                    let carNum: string = carSpeedData.value[index] || ''
                     if (!carSpeedData.value[index]) {
                       const templateData: any = data[index]
                       if (templateData.speed) {
