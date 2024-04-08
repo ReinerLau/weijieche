9e629d0ab63a6b7d0a64fafad3fab08d3bd8134b
ReinerLau <lk850593913@gmail.com>
2024-04-08 17:42:35 +0800

    ci: test

---
+++ __tests__/map/base.spec.ts
@@ -0,0 +1,69 @@
+import { describe, expect, it, vi } from 'vitest'
+import { initMap, map, jumpToCoordinate, clearMenu, backToCenter } from '@/shared/map/base'
+import { getCarInfo } from '@/api'
+import { currentCar } from '@/shared'
+
+vi.mock('@/api')
+
+const carInfo = { data: { longitude: 0, latitude: 0 } }
+vi.mocked(getCarInfo as any).mockImplementation(async () => {
+  return carInfo
+})
+
+describe('base', () => {
+  it('初始化地图', () => {
+    expect(map).toBeFalsy()
+
+    const mapEl = document.createElement('div')
+
+    initMap(mapEl)
+
+    expect(map).toBeTruthy()
+  })
+
+  it('地图跳转', () => {
+    const x = 0
+
+    const y = 0
+
+    jumpToCoordinate(0, 0)
+
+    const coordinate = map.getCenter()
+
+    expect(coordinate.x).toBe(x)
+
+    expect(coordinate.y).toBe(y)
+
+    const zoom = map.getZoom()
+
+    expect(zoom).toBe(18)
+  })
+
+  it('清空菜单', () => {
+    map.setMenuItems([
+      {
+        item: 'test'
+      }
+    ])
+
+    expect(map.getMenuItems()).toHaveLength(1)
+
+    clearMenu()
+
+    expect(map.getMenuItems()).toHaveLength(0)
+  })
+
+  it('回到中心', async () => {
+    jumpToCoordinate(100, 100)
+
+    currentCar.value = 'test'
+
+    await backToCenter()
+
+    const coordinate = map.getCenter()
+
+    expect(coordinate.x).toBe(0)
+
+    expect(coordinate.y).toBe(0)
+  })
+})
