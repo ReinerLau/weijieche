5dc865c2279e125872a38629101233c9428a71da
hye0228 <1261769443@qq.com>
2024-04-18 16:41:41 +0800

    test: 上传路线组件拆分

---
+++ src/components/FileUploadDialog.vue
@@ -0,0 +1,71 @@
+<template>
+  <ElDialog
+    v-model="fileUploadDialogVisible"
+    :title="t('shang-chuan-wen-jian')"
+    width="50vw"
+    align-center
+    @onClose="handleClose"
+  >
+    <ElUpload
+      ref="uploadRef"
+      :headers="{ Authorization: getToken() }"
+      :action="url"
+      :limit="1"
+      :auto-upload="false"
+      @success="handleSuccess"
+      :before-upload="beforeAvatarUpload"
+    >
+      <ElButton type="primary">{{ t('xuan-ze-lu-xian-wen-jian') }}</ElButton>
+    </ElUpload>
+    <ElButton size="large" type="primary" class="w-full" @Click="submitUpload">
+      {t('shang-chuan-lu-xian')}
+    </ElButton>
+  </ElDialog>
+</template>
+
+<script setup lang="ts">
+import { fileUploadDialogVisible } from '@/shared/map/file'
+import type { PointData } from '@/types'
+import { getToken } from '@/utils'
+import { ElMessage, type UploadInstance } from 'element-plus'
+import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+const emit = defineEmits<{
+  confirm: [PointData[]]
+}>()
+const uploadRef = ref<UploadInstance | null>(null)
+const url = `http://${window.location.host}/api/vehicle-task/v1/plan/parse`
+const submitUpload = () => {
+  if (uploadRef.value) {
+    uploadRef.value.submit()
+  }
+}
+
+const fileData = ref<PointData[]>()
+const handleSuccess = (response: { data: PointData[] }) => {
+  fileData.value = response.data
+  emit('confirm', fileData.value)
+}
+
+const beforeAvatarUpload = (rawFile: any) => {
+  const fileExtension = rawFile.name.split('.').pop().toLowerCase()
+  if (fileExtension !== 'plan') {
+    ElMessage.error(t('qing-shang-chuan-zheng-que-de-lu-xian-wen-jian'))
+    return false
+  }
+  ElMessage({
+    type: 'success',
+    message: t('shang-chuan-cheng-gong')
+  })
+  return true
+}
+
+const handleClose = () => {
+  // 关闭弹窗时重置上传组件的状态
+  if (uploadRef.value) {
+    uploadRef.value.clearFiles() // 清空上传的文件
+  }
+  fileUploadDialogVisible.value = false // 关闭弹窗
+}
+</script>
