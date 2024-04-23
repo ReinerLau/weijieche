f6de21bdead478ca07844fdae92a4501d0c07f03
hye0228 <1261769443@qq.com>
2024-04-23 14:24:10 +0800

    test: 实时路线测试用例

---
+++ __tests__/shared/map/realRoute.spec.ts
@@ -0,0 +1,53 @@
+import { currentCar, initMapLayerTool } from '@/shared'
+import {
+  initRealPath,
+  initRealPathLayer,
+  isReal,
+  realPathLayer,
+  realPathPoints
+} from '@/shared/map/realRoute'
+import { ConnectorLine, Marker } from 'maptalks'
+import { beforeEach, describe, expect, it } from 'vitest'
+
+const testData = {
+  latitude: 22.56121026,
+  longitude: 113.4821599,
+  heading: '30',
+  robotCode: '003',
+  rid: '003',
+  robotid: '003'
+}
+
+const testData1 = {
+  latitude: 22.56122,
+  longitude: 113.4822,
+  heading: '30',
+  robotCode: '003',
+  rid: '003',
+  robotid: '003'
+}
+
+describe('巡逻路线展示', () => {
+  beforeEach(() => {
+    initMapLayerTool()
+    initRealPathLayer()
+  })
+
+  it('新建路线图层', () => {
+    expect(realPathLayer).not.toBeUndefined()
+  })
+
+  it('initRealPath', () => {
+    isReal.value = true
+    currentCar.value = '003'
+    initRealPath(testData)
+    expect(realPathLayer.getGeometries()[0]).instanceOf(Marker)
+    expect(realPathPoints.length).toBe(1)
+  })
+
+  it('realPathPoints.length >= 2', () => {
+    initRealPath(testData1)
+    expect(realPathPoints.length).toBe(2)
+    expect(realPathLayer.getGeometries()[1]).instanceOf(ConnectorLine)
+  })
+})
