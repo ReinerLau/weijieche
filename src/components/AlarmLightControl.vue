beef7475e506d83b3e8b27e0a2757cdd122a82ae
hye0228 <1261769443@qq.com>
2024-05-24 17:38:38 +0800

    feat: 警报灯接口修改

--- src/components/AlarmLightControl.vue
+++ src/components/AlarmLightControl.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
-    <div class="flex justify-between items-center">
-      <span>{{ t('jing-bao-deng') }}</span>
-      <el-switch :model-value="alarmLight" @change="handleAlarmLight"></el-switch>
-    </div>
+    <el-row :gutter="24" class="w-full">
+      <el-col :xs="24" :sm="24">
+        <div class="flex justify-between items-center">
+          <span>{{ t('jing-bao-deng') }}</span>
+          <el-switch :model-value="alarmLight" @change="handleAlarmLight"></el-switch></div></el-col
+    ></el-row>
   </div>
 </template>
 
 <script setup lang="ts">
-import { patrolingCruise } from '@/api/control'
+import { controlAlarmLight } from '@/api/control'
 import { currentCar, haveCurrentCar } from '@/shared'
 import { ref } from 'vue'
 import { useI18n } from 'vue-i18n'
@@ -16,17 +18,14 @@ import { useI18n } from 'vue-i18n'
 const { t } = useI18n()
 //警告灯是否开启
 const alarmLight = ref(false)
-function handleAlarmLight(value: boolean) {
+async function handleAlarmLight(value: boolean) {
   alarmLight.value = !alarmLight.value
   if (haveCurrentCar()) {
     const data = {
       code: currentCar.value,
-      param1: 8,
-      param2: value ? 1 : 0,
-      param3: 255,
-      param4: 255
+      type: value ? 1 : 0
     }
-    patrolingCruise(data)
+    await controlAlarmLight(data)
   } else {
     alarmLight.value = false
   }
