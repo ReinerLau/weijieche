a74c73415faab695b2391d5c4b8e4f8b3fb99966
hye0228 <1261769443@qq.com>
2024-05-24 17:39:24 +0800

    feat: 音乐列表、状态

--- src/components/MusicControl.vue
+++ src/components/MusicControl.vue
@@ -7,6 +7,26 @@
         </el-button>
       </template>
     </div>
+    <div class="flex justify-start flex-col mb-4">
+      <el-button @click="onClickStatus" class="mb-2">{{ t('zhuang-tai-fan-hui') }}</el-button>
+      <el-row :gutter="24" class="w-full">
+        <el-col :xs="12" :sm="12">播放状态：{{ musicStatus }}</el-col>
+        <el-col :xs="12" :sm="12">播放模式：{{ musicMode }}</el-col>
+      </el-row>
+      <el-row :gutter="24" class="w-full">
+        <el-col :xs="12" :sm="12">音量值：{{ musicVolumeValue }}</el-col>
+        <el-col :xs="12" :sm="12">当前播放索引：{{ musicIndex }}</el-col>
+      </el-row>
+      <el-row :gutter="24" class="w-full">
+        <el-col :xs="24" :sm="24">当前歌名：{{ musicName }}</el-col>
+      </el-row>
+    </div>
+    <div class="flex justify-start flex-col mb-4">
+      <el-button @click="onClickMusicList" class="mb-2">{{ t('yin-le-lie-biao') }}</el-button>
+      <el-row :gutter="24" class="w-full">
+        <el-col :xs="24" :sm="24">当前歌曲总数：{{ musicListNumber }}</el-col>
+      </el-row>
+    </div>
     <div class="flex items-center justify-between">
       <div>{{ t('xuan-ze-di') }}{{ musicNum }}{{ t('shou-yin-le') }}</div>
       <el-input-number v-model="musicNum" :min="1" @change="changeMusic" />
@@ -16,13 +36,12 @@
 
 <script setup lang="ts">
 import { patrolingCruise } from '@/api/control'
+import { musicList, musicMessage } from '@/composables/useUpperControl'
 import { currentCar, haveCurrentCar } from '@/shared'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
-
 // 国际化
 const { t } = useI18n()
-
 const musicNum = ref(1)
 // 按钮组合
 const buttonList = [
@@ -38,11 +57,6 @@ const buttonList = [
     value: 0,
     content: t('ting-zhi')
   },
-  // {
-  //   value: '12',
-  //   content: t('lie-biao')
-  // },
-
   {
     value: 6,
     content: t('yin-liang-0')
@@ -71,11 +85,68 @@ const buttonList = [
     value: 10,
     content: t('quan-bu-xun-huan')
   }
-  // {
-  //   value: '11',
-  //   content: t('zhuang-tai-fan-hui')
-  // }
 ]
+
+const musicVolumeValue = ref('')
+const musicIndex = ref('')
+const musicMode = ref('')
+const musicName = ref('')
+const musicStatus = ref('')
+watch(musicMessage, () => {
+  if (musicMessage.value) {
+    const { volumeValue, index, mode, name, status } = musicMessage.value
+    musicVolumeValue.value = volumeValue
+    musicIndex.value = index
+    musicMode.value = mode
+    musicName.value = name
+    musicStatus.value = status
+  }
+})
+
+const musicListNumber = ref(0)
+
+watch(musicList, () => {
+  if (musicList.value) {
+    musicListNumber.value = musicList.value[0]['total']
+  }
+})
+
+async function onClickStatus() {
+  if (haveCurrentCar()) {
+    const data = {
+      code: currentCar.value,
+      param1: 4,
+      param2: 11,
+      param3: 255,
+      param4: 255
+    }
+    await patrolingCruise(data)
+    if (musicMessage.value) {
+      const { volumeValue, index, mode, name, status } = musicMessage.value
+      musicVolumeValue.value = volumeValue
+      musicIndex.value = index
+      musicMode.value = mode
+      musicName.value = name
+      musicStatus.value = status
+    }
+  }
+}
+
+async function onClickMusicList() {
+  if (haveCurrentCar()) {
+    const data = {
+      code: currentCar.value,
+      param1: 4,
+      param2: 12,
+      param3: 255,
+      param4: 255
+    }
+    await patrolingCruise(data)
+    if (musicList.value) {
+      musicListNumber.value = musicList.value[0]['total']
+    }
+  }
+}
 
 async function onClickMusic(value: number) {
   if (haveCurrentCar()) {
---
+++ src/composables/useUpperControl.ts
@@ -0,0 +1,117 @@
+import { initWebSocket } from '@/utils'
+import { ElMessage } from 'element-plus'
+import { onBeforeUnmount, ref, watch } from 'vue'
+import { useI18n } from 'vue-i18n'
+export const musicMessage = ref()
+export const musicList = ref([])
+export const useUpperControl = () => {
+  // 国际化
+  const { t } = useI18n()
+
+  // 标记是否已经连接 websocket
+  const isConnectedWS = ref(false)
+
+  const isOpenFeedback = ref(false)
+  let ws: WebSocket | undefined
+
+  const birStatus = ref('')
+  const lightStatus = ref('')
+
+  //断开重连定时器
+  let reconnectInterval: number | NodeJS.Timer | null = null
+
+  // 监听到选择车辆后连接 websocket
+  watch(isOpenFeedback, () => {
+    if (isOpenFeedback.value) {
+      tryCloseWS()
+      connectWebSocket()
+    }
+  })
+
+  // 关闭页面前先关闭 websocket
+  onBeforeUnmount(tryCloseWS)
+
+  // 关闭 websocket
+  function tryCloseWS() {
+    if (ws) {
+      ws.close()
+      ws = undefined
+    }
+    clearReconnectInterval()
+  }
+
+  // 清除重连定时器
+  function clearReconnectInterval() {
+    if (reconnectInterval) {
+      clearInterval(reconnectInterval)
+      reconnectInterval = null
+    }
+  }
+
+  // 连接 WebSocket
+  function connectWebSocket() {
+    ws = initWebSocket('/websocket/patroling/feedback', {
+      onmessage: (event: MessageEvent<any>) => {
+        const data = JSON.parse(event.data)
+        if (data.type === 4) {
+          if (data.code === 'ADAS') {
+            musicMessage.value = data.message
+          } else if (data.code === 'ADAG') {
+            musicList.value = data.message
+          }
+        } else if (data.type === 6) {
+          birStatus.value = data.message
+        } else if (data.type === 7) {
+          lightStatus.value = data.message
+        }
+      },
+
+      onopen: () => {
+        isConnectedWS.value = true
+        ElMessage({
+          type: 'success',
+          message: t('jian-ting-zhuang-tai-lian-jie-cheng-gong')
+        })
+        clearReconnectInterval()
+      },
+      onclose: () => {
+        isConnectedWS.value = false
+        ElMessage({
+          type: 'warning',
+          message: t('jian-ting-zhuang-tai-lian-jie-duan-kai')
+        })
+        // 断开后每隔一段时间重新连接
+        startReconnectInterval()
+      },
+      onerror: () => {
+        isConnectedWS.value = false
+        ElMessage({
+          type: 'warning',
+          message: t('jian-ting-zhuang-tai-lian-jie-cuo-wu')
+        })
+        // 出错后每隔一段时间重新连接
+        startReconnectInterval()
+      }
+    })
+  }
+
+  // 开始重连定时器
+  function startReconnectInterval() {
+    if (!reconnectInterval) {
+      reconnectInterval = setInterval(() => {
+        tryCloseWS()
+        ElMessage({
+          type: 'warning',
+          message: t('websocket-duan-kai-zhong-lian-zhong')
+        })
+        connectWebSocket()
+      }, 5000) // 每隔5秒重新连接
+    }
+  }
+
+  return {
+    birStatus,
+    lightStatus,
+    isOpenFeedback
+  }
+}
